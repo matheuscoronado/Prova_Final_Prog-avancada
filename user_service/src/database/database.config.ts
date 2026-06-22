806312b47e83dd4120fd6b4b4dfd81ba502da217
Matheus Coronado <matheuscoronado48@gmail.com>
2026-06-22 19:36:35 -0300

    feat(database): configurar conexão com MySQL via Sequelize

---
+++ user_service/src/database/database.config.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  SequelizeModuleOptions,
+  SequelizeOptionsFactory,
+} from '@nestjs/sequelize';
+import { User } from '../user/user.entity';
+
+@Injectable()
+export class DatabaseConfig implements SequelizeOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createSequelizeOptions(): SequelizeModuleOptions {
+    return {
+      dialect: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      synchronize: true,
+      autoLoadModels: true,
+      models: [User],
+    };
+  }
+}
+
