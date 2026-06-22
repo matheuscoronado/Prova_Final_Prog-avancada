c780902b0baae89d378ae7c6966f45c023e07b50
Matheus Coronado <matheuscoronado48@gmail.com>
2026-06-22 19:57:52 -0300

    feat(user): criar módulo com conexão TCP ao user_service

---
+++ api_prova/src/user/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UserController } from './user.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3001,
+        },
+      },
+    ]),
+  ],
+  controllers: [UserController],
+})
+export class UserModule {}
