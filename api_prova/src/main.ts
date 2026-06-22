1ccc801be09d375b96b17a546f0e816d405346a1
Matheus Coronado <matheuscoronado48@gmail.com>
2026-06-22 19:52:09 -0300

    feat(main): habilitar CORS e configurar documentação Swagger

--- api_prova/src/main.ts
+++ api_prova/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Prova - Usuários')
+    .setDescription('Documentação dos endpoints de Login, Criação e Consulta de usuários')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+  console.log('API Gateway rodando em http://localhost:3000');
+  console.log('Swagger disponível em http://localhost:3000/docs');
 }
 bootstrap();
